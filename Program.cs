dcd40069799c00a600e78bcd8d8b1813623a9af1
Crindere-Dev <69606716+Crindere-Dev@users.noreply.github.com>
2021-08-02 06:56:34 +0100

    v

--- Program.cs
+++ Program.cs
@@ -1,24 +1,49 @@
 ﻿using System;
-                    
+
 public class Program
 {
     public static void Main()
     {
         Console.WriteLine("Crin: Welcome! Popcorn is £10, Please insert cash here: ");
-        
+
         int cash =     Convert.ToInt32(Console.ReadLine());
-        
+
         if (cash < 10)
         {
         Console.WriteLine("Card Declined.");
         Console.WriteLine("Crin: Sorry Mate, guess you can't enjoy your popcorn.");
-        
-        }    
+
+      }else if (cash > 10) {
+        int result = cash - 10;
+
+        Console.WriteLine($"Here's your cash :D");
+        Typewrite($"Crin hands you {result}");
+
+        }
+
+
+
+
+
+
         else
         {
         Console.WriteLine("Card Approved.");
         Console.WriteLine("Crin: Enjoy!");
-        }    
-        
+        }
+
     }
+        static void Typewrite(string message)
+        {
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.Write(message[i]);
+                System.Threading.Thread.Sleep(60);
+            }
+
+        }
+
+
+
+
 }
